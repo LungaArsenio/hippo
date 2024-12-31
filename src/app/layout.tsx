9bf75a77f161193d594c9eec3b28acf8433dbb80
LungaArsenio <lungaarsenio0@gmail.com>
2024-12-30 23:59:24 -0600

    cleaaning unnecessary starter files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className="relative h-full font-sans antialiased">
         <main className="relative flex flex-col min-h-screen">
+          <Navbar />
           {/** Adding flex-grow flex-1 helps specify elements to take up available space
            * Even if multiple elements have flex-grow: 1, they will share the extra available space
            * when used flex-1 to all elements in a container it makes each element:
